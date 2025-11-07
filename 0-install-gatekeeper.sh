35e9fff9bcdcf13ac5ef374fe09e5a11ac4c4e54
Tatiana-isala <ivverstone@gmail.com>
2025-11-07 17:22:37 +0100

    Delete 0-install-gatekeeper.sh

--- 0-install-gatekeeper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Installe Gatekeeper si non présent
-if ! kubectl get ns gatekeeper-system; then
-  kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/deploy/gatekeeper.yaml
-  sleep 10 # Attente du déploiement
-fi
